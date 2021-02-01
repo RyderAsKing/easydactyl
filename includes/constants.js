a7a61820885e60b55682bd9d1a3454e36f51e475
RyderAsKing <50543053+RyderAsKing@users.noreply.github.com>
2021-02-01 09:16:27 +0530

    Example constants

--- includes/constants.js
+++ includes/constants.js
@@ -4,3 +4,5 @@ const config = require("../config.json");
 const con = require("../includes/db.js");
 const easyLogger = require("../includes/easyLogger.js");
 const fs = require('fs');
+
+// This is just an example of required constants while creating a new command
